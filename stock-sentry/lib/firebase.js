3a52d8855690209927f3905fd628185aab4b6160
Sarah Pendhari <sarahpendhari@gmail.com>
2024-08-02 22:47:08 +0530

    Update

---
+++ stock-sentry/lib/firebase.js
@@ -0,0 +1,22 @@
+// lib/firebase.js
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBnGC5GnZisIR6LEbNwWfw_LfzP4uxaLK4",
+    authDomain: "stocksentry-c3868.firebaseapp.com",
+    projectId: "stocksentry-c3868",
+    storageBucket: "stocksentry-c3868.appspot.com",
+    messagingSenderId: "730095808356",
+    appId: "1:730095808356:web:126e2a5d3cbe68fe79d471",
+    measurementId: "G-6QWDCR9PTJ"
+  };
+
+  
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+export { db, storage };
